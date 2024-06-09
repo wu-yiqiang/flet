cb1ad59ca1033b22bf3897b08cd636041baaf1da
wu-yiqiang <wu_yiqiang@outlook.com>
2024-06-10 00:35:25 +0800

    绘制登录页面

--- lib/pages/Login/login.dart
+++ lib/pages/Login/login.dart
@@ -2,34 +2,71 @@ import "package:get/get.dart";
 import 'package:flutter/material.dart';
 import 'package:flet/pages/Login/controller/login_controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginPage extends StatelessWidget {
   LoginController loginController = Get.put(LoginController());
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
         child: Container(
-          child:  Center(
-            child: Column(
-              children: [
-                Container(
-                  child: Text(
-                    "登录",
-                    style:
-                    TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w700),
+          width: 360.w,
+          height: 690.h,
+          padding: EdgeInsets.all(20),
+          color: Color.fromRGBO(52,122,240,1),
+          child: Scaffold(
+            backgroundColor: Color.fromRGBO(52,122,240,1),
+            body: Container(
+              padding: EdgeInsets.all(20),
+              decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.all(Radius.circular(6.0)),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset("assets/images/logo.png",
+                    width: 100.0,
                   ),
-                ),
-                Text("sadas"),
-                Text("sadas")
-              ],
-            ),
-          ),
-          width: 300.w,
-          color: Colors.green,
-        ),
-        color: Colors.white,
-      ),
+                  SizedBox(height: 50),
+                  TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: 'username'.tr,
+                        hintText: 'username-tips'.tr,
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder()),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "password".tr,
+                        hintText: "password-tips".tr,
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder()),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton.icon(
+                        icon: SvgPicture.asset("assets/svg/register.svg",
+                            width: 26, height: 26),
+                        label: Text("register".tr),
+                        onPressed: () {},
+                      ),
+                      ElevatedButton.icon(
+                        icon: SvgPicture.asset("assets/svg/login.svg",
+                            width: 26, height: 26),
+                        label: Text("login".tr),
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ))
+        )
     );
   }
 }
--- lib/translation/translation.dart
+++ lib/translation/translation.dart
@@ -6,10 +6,23 @@ class Messages extends Translations {
   // TODO: implement keys
   Map<String, Map<String, String>> get keys => {
     'zh_CN': {
+      "username": "用户名",
+      'username-tips': "用户名或邮箱",
+      "password": "密码",
+      "password-tips": "登录密码",
+      "register": "注册",
+      "login": "登录",
       'more': "更多",
       "more-services": "更多服务",
+      "username": "用户名",
     },
     'en_US': {
+      "username": "username",
+      'username-tips': "username or email",
+      "password": "password",
+      "password-tips": "login password",
+      "register": "register",
+      "login": "login",
       'more': "more",
       "more-services": "more services",
     }
